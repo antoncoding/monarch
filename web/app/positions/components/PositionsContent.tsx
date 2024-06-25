a757ba375b850e1d833bc2ff5fd4710fc03fdc43
Anton Cheng <antonassocareer@gmail.com>
2024-06-25 15:56:33 +0800

    fix: key error

--- web/app/positions/components/PositionsContent.tsx
+++ web/app/positions/components/PositionsContent.tsx
@@ -144,140 +144,128 @@ export default function Positions() {
                   }, BigInt(0));
 
                   return (
-                    <>
-                      <tr key={index.toFixed()}>
-                        {/* network */}
-                        <td>
-                          <div className="flex justify-center">
-                            {networkImg ? (
-                              <Image src={networkImg} alt="icon" width="18" height="18" />
-                            ) : null}
-                          </div>
-                        </td>
+                    <tr key={index.toFixed()}>
+                      {/* network */}
+                      <td>
+                        <div className="flex justify-center">
+                          {networkImg ? (
+                            <Image src={networkImg} alt="icon" width="18" height="18" />
+                          ) : null}
+                        </div>
+                      </td>
 
-                        {/* id */}
-                        <td>
-                          <div className="flex justify-center font-monospace text-xs">
-                            <a
-                              className="group flex items-center gap-1 no-underline hover:underline"
-                              href={getMarketURL(
-                                position.market.uniqueKey,
-                                position.market.morphoBlue.chain.id,
-                              )}
-                              target="_blank"
-                            >
-                              <p>{position.market.uniqueKey.slice(2, 8)} </p>
-                              <p className="opacity-0 group-hover:opacity-100">
-                                <ExternalLinkIcon />
-                              </p>
-                            </a>
-                          </div>
-                        </td>
-
-                        {/* supply */}
-                        <td>
-                          <div>
-                            <div className="flex items-center justify-center gap-1">
-                              <p>
-                                {formatReadable(
-                                  formatBalance(
-                                    position.supplyAssets,
-                                    position.market.loanAsset.decimals,
-                                  ),
-                                )}{' '}
-                              </p>
-                              <p> {position.market.loanAsset.symbol} </p>
-                              {loanImg ? (
-                                <Image src={loanImg} alt="icon" width="18" height="18" />
-                              ) : null}
-                            </div>
-                          </div>
-                        </td>
-
-                        {/* collateral */}
-                        <td>
-                          <div className="flex items-center justify-center gap-1">
-                            <div> {position.market.collateralAsset.symbol} </div>
-                            {collatImg ? (
-                              <Image src={collatImg} alt="icon" width="18" height="18" />
-                            ) : null}
-                            <p> {} </p>
-                          </div>
-                        </td>
-
-                        <td>
-                          <div className="flex items-center justify-center gap-1">
-                            <p> {formatBalance(position.market.lltv, 16)} % </p>
-                          </div>
-                        </td>
-
-                        <td>
-                          <div className="flex items-center justify-center gap-1">
-                            {hasRewards && (
-                              <p> {formatReadable(formatBalance(claimableMorpho, 18))} </p>
+                      {/* id */}
+                      <td>
+                        <div className="font-monospace flex justify-center text-xs">
+                          <a
+                            className="group flex items-center gap-1 no-underline hover:underline"
+                            href={getMarketURL(
+                              position.market.uniqueKey,
+                              position.market.morphoBlue.chain.id,
                             )}
-                            {hasRewards && (
-                              <Image src={MORPHO_LOGO} alt="icon" width="18" height="18" />
-                            )}
-                            {!hasRewards && <p> - </p>}
-                          </div>
-                        </td>
-
-                        <td>
-                          <div className="flex items-center justify-center gap-1">
-                            {hasRewards && (
-                              <p> {formatReadable(formatBalance(pendingMorpho, 18))} </p>
-                            )}
-                            {hasRewards && (
-                              <Image src={MORPHO_LOGO} alt="icon" width="18" height="18" />
-                            )}
-                            {!hasRewards && <p> - </p>}
-                          </div>
-                        </td>
-
-                        {/* APYs */}
-                        <td className="z-50">
-                          {formatReadable(position.market.dailyApys.netSupplyApy * 100)}
-                          {/* <p>{formatReadable(position.market.weeklyApys.netSupplyApy * 100)}</p> */}
-                        </td>
-
-                        {/* percentage */}
-                        <td>
-                          <p className="opacity-70">
-                            {formatReadable(
-                              (Number(position.supplyAssets) /
-                                Number(position.market.state.supplyAssets)) *
-                                100,
-                            )}
-                            %
-                          </p>
-                        </td>
-
-                        <td>
-                          <button
-                            type="button"
-                            aria-label="Supply"
-                            className="bg-hovered items-center justify-between rounded-sm p-2 text-xs duration-300 ease-in-out hover:scale-110  hover:bg-orange-500 "
-                            onClick={() => {
-                              setShowModal(true);
-                              setSelectedPosition(position);
-                            }}
+                            target="_blank"
                           >
-                            Withdraw
-                          </button>
-                        </td>
-                      </tr>
+                            <p>{position.market.uniqueKey.slice(2, 8)} </p>
+                            <p className="opacity-0 group-hover:opacity-100">
+                              <ExternalLinkIcon />
+                            </p>
+                          </a>
+                        </div>
+                      </td>
 
-                      {/* {expandedRowId === position.market.uniqueKey && (
-                        <tr>
-                           <td className="collaps-viewer bg-hovered" colSpan={7}>
-                            <div className="flex w-full p-12 text-center">
-                              HOLA
-                            </div>
-                          </td>
-                        </tr>
-                      )} */}
-                    </>
+                      {/* supply */}
+                      <td>
+                        <div>
+                          <div className="flex items-center justify-center gap-1">
+                            <p>
+                              {formatReadable(
+                                formatBalance(
+                                  position.supplyAssets,
+                                  position.market.loanAsset.decimals,
+                                ),
+                              )}{' '}
+                            </p>
+                            <p> {position.market.loanAsset.symbol} </p>
+                            {loanImg ? (
+                              <Image src={loanImg} alt="icon" width="18" height="18" />
+                            ) : null}
+                          </div>
+                        </div>
+                      </td>
+
+                      {/* collateral */}
+                      <td>
+                        <div className="flex items-center justify-center gap-1">
+                          <div> {position.market.collateralAsset.symbol} </div>
+                          {collatImg ? (
+                            <Image src={collatImg} alt="icon" width="18" height="18" />
+                          ) : null}
+                          <p> {} </p>
+                        </div>
+                      </td>
+
+                      <td>
+                        <div className="flex items-center justify-center gap-1">
+                          <p> {formatBalance(position.market.lltv, 16)} % </p>
+                        </div>
+                      </td>
+
+                      <td>
+                        <div className="flex items-center justify-center gap-1">
+                          {hasRewards && (
+                            <p> {formatReadable(formatBalance(claimableMorpho, 18))} </p>
+                          )}
+                          {hasRewards && (
+                            <Image src={MORPHO_LOGO} alt="icon" width="18" height="18" />
+                          )}
+                          {!hasRewards && <p> - </p>}
+                        </div>
+                      </td>
+
+                      <td>
+                        <div className="flex items-center justify-center gap-1">
+                          {hasRewards && (
+                            <p> {formatReadable(formatBalance(pendingMorpho, 18))} </p>
+                          )}
+                          {hasRewards && (
+                            <Image src={MORPHO_LOGO} alt="icon" width="18" height="18" />
+                          )}
+                          {!hasRewards && <p> - </p>}
+                        </div>
+                      </td>
+
+                      {/* APYs */}
+                      <td className="z-50">
+                        {formatReadable(position.market.dailyApys.netSupplyApy * 100)}
+                        {/* <p>{formatReadable(position.market.weeklyApys.netSupplyApy * 100)}</p> */}
+                      </td>
+
+                      {/* percentage */}
+                      <td>
+                        <p className="opacity-70">
+                          {formatReadable(
+                            (Number(position.supplyAssets) /
+                              Number(position.market.state.supplyAssets)) *
+                              100,
+                          )}
+                          %
+                        </p>
+                      </td>
+
+                      <td>
+                        <button
+                          type="button"
+                          aria-label="Supply"
+                          className="bg-hovered items-center justify-between rounded-sm p-2 text-xs duration-300 ease-in-out hover:scale-110  hover:bg-orange-500 "
+                          onClick={() => {
+                            setShowModal(true);
+                            setSelectedPosition(position);
+                          }}
+                        >
+                          Withdraw
+                        </button>
+                      </td>
+                    </tr>
                   );
                 })}
               </tbody>
