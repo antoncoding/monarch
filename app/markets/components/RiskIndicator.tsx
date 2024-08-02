93dd5777043413586222764fcc60f5a1b6ac074b
Anton Cheng <antonassocareer@gmail.com>
2024-08-03 00:42:56 +0800

    refactor: risk indicators

--- app/markets/components/RiskIndicator.tsx
+++ app/markets/components/RiskIndicator.tsx
@@ -8,57 +8,60 @@ type RiskFlagProps = {
 };
 
 export function RiskIndicator({ level, description }: RiskFlagProps) {
-    return (
-      <Tooltip content={description}>
-        <div className="gap flex">
-          {level === 'green' && <div className="h-4 w-2 bg-green-500" />}
-          {level === 'yellow' && <div className="h-4 w-2 bg-yellow-500" />}
-          {level === 'red' && <div className="h-4 w-2 bg-red-500" />}
-        </div>
-      </Tooltip>
-    );
+  return (
+    <Tooltip content={description}>
+      <div className="gap flex">
+        {level === 'green' && <div className="h-4 w-2 bg-green-500" />}
+        {level === 'yellow' && <div className="h-4 w-2 bg-yellow-500" />}
+        {level === 'red' && <div className="h-4 w-2 bg-red-500" />}
+      </div>
+    </Tooltip>
+  );
+}
+
+export function RiskIndicatorFromWarning({ market, category, greeDescription, yellowDescription, redDescription }: {
+  market: Market;
+  category: WarningCategory;
+  greeDescription: string;
+  yellowDescription: string;
+  redDescription: string;
+}) {
+  const warnings = market.warningsWithDetail.filter((w) => w.category === category);
+  if (warnings.length === 0) {
+    return <RiskIndicator level="green" description={greeDescription} />;
   }
-  
+  if (warnings.some((warning) => warning.level === 'alert')) {
+    return <RiskIndicator level="red" description={redDescription} />;
+  } else return <RiskIndicator level="yellow" description={yellowDescription} />;
+
+}
 
 export function MarketAssetIndicator({ market }: { market: Market }) {
-    const warnings = market.warningsWithDetail.filter(w => w.category === WarningCategory.asset);
-    
-    if (warnings.length === 0) {
-        return <RiskIndicator level="green" description="Legit Assets" />;
-    }
-
-    if (warnings.some((warning) => warning.level === 'alert')) {
-        return <RiskIndicator level="red" description="Dangerous assets" />;
-    }
-
-    else return <RiskIndicator level="yellow" description="Some warning flagged with the assets" />;
-
+  return <RiskIndicatorFromWarning
+    market={market}
+    category={WarningCategory.asset}
+    greeDescription="Recognized assets"
+    yellowDescription="Some warning flagged with the assets"
+    redDescription="Dangerous assets"
+  />
 }
 
 export function MarketOracleIndicator({ market }: { market: Market }) {
-    const warnings = market.warningsWithDetail.filter(w => w.category === WarningCategory.oracle);
-    
-    if (warnings.length === 0) {
-        return <RiskIndicator level="green" description="Oracle seems OK!" />;
-    }
-
-    if (warnings.some((warning) => warning.level === 'alert')) {
-        return <RiskIndicator level="red" description="One or more warnings on oracle." />;
-    }
-
-    else return <RiskIndicator level="yellow" description="One or more warnings on oracle." />;
-
+  return <RiskIndicatorFromWarning
+    market={market}
+    category={WarningCategory.oracle}
+    greeDescription="Recognized oracles"
+    yellowDescription="Some warning flagged with the oracle"
+    redDescription="Some alert flagged with the oracle"
+  />
 }
 
 export function MarketDebtIndicator({ market }: { market: Market }) {
-    const warnings = market.warningsWithDetail.filter(w => w.category === WarningCategory.debt);
-
-    if (warnings.length === 0) {
-        return <RiskIndicator level="green" description="No bad debt" />;
-    }
-    if (warnings.some((warning) => warning.level === 'alert')) {
-        return <RiskIndicator level="red" description="Bad debt has occurred" />;
-    }
-
-    else return <RiskIndicator level="yellow" description="Bad debt higher than 1%" />;
+  return <RiskIndicatorFromWarning
+    market={market}
+    category={WarningCategory.debt}
+    greeDescription="No bad debt"
+    yellowDescription="Bad debt has occurred"
+    redDescription="Bad debt higher than 1% of supply"
+  />
 }
