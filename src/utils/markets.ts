f6d7db1f55294372fbf4685e7a4c61bd476a433e
antoncoding <antonassocareer@gmail.com>
2025-06-29 21:12:52 +0800

    chore: blacklist

--- src/utils/markets.ts
+++ src/utils/markets.ts
@@ -1,6 +1,7 @@
 // Blacklisted markets by uniqueKey
 export const blacklistedMarkets = [
   '0x8eaf7b29f02ba8d8c1d7aeb587403dcb16e2e943e4e2f5f94b0963c2386406c9', // PAXG / USDC market with wrong oracle
+  '0x7e79c25831c97175922df132d09b02f93103a2306b1d71e57a7714ddd4c15d13', // Relend USDC / USDC: Should be considered unrecoverable
 ];
 
 // Market specially whitelisted by Monarch, lowercase
