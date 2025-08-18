306f0c8f1a8de6c86d49725445bfc90bf621ad53
antoncoding <antonassocareer@gmail.com>
2025-08-18 18:21:28 +0800

    chore: add back 9

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -26,7 +26,7 @@ export default function Input({
 }: InputProps): JSX.Element {
   // State for the input text
   const [inputAmount, setInputAmount] = useState<string>(
-    value ? formatBalance(value, decimals).toString() : '',
+    value ? formatBalance(value, decimals).toString() : '0',
   );
 
   // Update input text when value prop changes
