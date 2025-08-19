9152523f4ee5b3130f7a14fc55b01ff61f65094b
antoncoding <antonassocareer@gmail.com>
2025-08-19 12:52:44 +0800

    chore: remove unncessary configs

--- next.config.js
+++ next.config.js
@@ -24,15 +24,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        punycode: false,
-      }
-    }
-    return config
-  },
 };
 
 module.exports = nextConfig;
