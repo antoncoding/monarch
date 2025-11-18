d36c2f7627adf479f84f383620655358de9684c5
antoncoding <antonassocareer@gmail.com>
2025-11-18 17:17:39 -0300

    chore: lint

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,4 @@
 import { useCallback, useState, useEffect } from 'react';
-import { Cross2Icon } from '@radix-ui/react-icons';
 
 import { parseUnits } from 'viem';
 import { formatBalance } from '@/utils/balance';
