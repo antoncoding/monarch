6bb89f7fe9566b64326e3705333981127be53bfc
antoncoding <antonassocareer@gmail.com>
2025-12-05 19:12:20 +0800

    chore: roundedness adjustment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,11 +74,6 @@ const config: Config = {
           foreground: 'hsl(var(--card-foreground))',
         },
       },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
       fontFamily: {
         inter: ['var(--font-inter)'],
         zen: ['var(--font-zen)'],
