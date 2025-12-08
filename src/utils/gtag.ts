198224e6205a47b3ca9752b44509327ccdf99e78
antoncoding <antonassocareer@gmail.com>
2025-12-09 00:30:08 +0800

    misc: feedback reviews

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -10,15 +10,26 @@ export const logPageview = (url: string) => {
   if (!isClient()) {
     return;
   }
+  const gtag = (window as WindowWithGtag).gtag;
+  if (!gtag) {
+    return;
+  }
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  (window as WindowWithGtag).gtag('config', GOOGLE_ANALYTICS_ID, {
+  gtag('config', GOOGLE_ANALYTICS_ID, {
     page_path: url,
   });
 };
 
 export const logEvent = ({ action, category, label, value }: { action: string; category: string; label: string; value: number }) => {
+  if (!isClient()) {
+    return;
+  }
+  const gtag = (window as WindowWithGtag).gtag;
+  if (!gtag) {
+    return;
+  }
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  (window as WindowWithGtag).gtag('event', action, {
+  gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value,
