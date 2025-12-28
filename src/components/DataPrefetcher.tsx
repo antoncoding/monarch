5d78bbcf415bea2c45e25b65db5d89483fd80054
antoncoding <antonassocareer@gmail.com>
2025-12-28 12:51:08 +0800

    docs: simple docstring

--- src/components/DataPrefetcher.tsx
+++ src/components/DataPrefetcher.tsx
@@ -4,6 +4,11 @@ import { useMarketsQuery } from '@/hooks/queries/useMarketsQuery';
 import { useTokensQuery } from '@/hooks/queries/useTokensQuery';
 import { useMerklCampaignsQuery } from '@/hooks/queries/useMerklCampaignsQuery';
 
+/**
+ * Triggeres data prefetching for markets, tokens, and Merkl campaigns.
+ * These hooks use React Query under the hood, which will cache the data for future use.
+ * @returns 
+ */
 export function DataPrefetcher() {
   useMarketsQuery();
   useTokensQuery();
