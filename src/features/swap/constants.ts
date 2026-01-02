29fdd1a6f67c4976a771fa707f19bee7465d0d16
antoncoding <antonassocareer@gmail.com>
2026-01-02 13:13:35 +0800

    misc: app code

--- src/features/swap/constants.ts
+++ src/features/swap/constants.ts
@@ -1,7 +1,7 @@
 /**
  * Application identifier for CoW Protocol integration
  */
-export const SWAP_APP_CODE = 'monarch-swap';
+export const SWAP_APP_CODE = 'monarchlend';
 
 /**
  * Default slippage tolerance as a percentage (0.5 = 0.5%)
