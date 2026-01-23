089311edbdd20f99482efe94c09c3c7f4fd34642
antoncoding <antonassocareer@gmail.com>
2026-01-23 15:05:03 +0800

    chore: id

--- src/hooks/useMarketCampaigns.ts
+++ src/hooks/useMarketCampaigns.ts
@@ -8,7 +8,7 @@ const BLACKLISTED_CAMPAIGN_IDS: string[] = [
   // https://app.merkl.xyz/opportunities/base/MORPHOSUPPLY_SINGLETOKEN/0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913
   '0x4b5aa0f66eb6a63e3b761de8fbbcc8154d568086c1234ba58516f3263a79200a',
   // https://app.merkl.xyz/opportunities/base/MORPHOSUPPLY_SINGLETOKEN/0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913WHITELIST_PER_PROTOCOL
-  '0x3dd9c2e06ceb004c58e634c18c27de6ad05ae91804144f57130c9394f9b87633',
+  '0x97380b45eed593b3108275de15ba89e452eaffeb04f0ffc7d8f131cf8c70f7a3',
 ];
 
 type UseMarketCampaignsReturn = {
