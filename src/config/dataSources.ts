a90c60314fecccff56bd25f0ceefb2790c4d9d29
antoncoding <antonassocareer@gmail.com>
2026-02-17 12:18:00 +0800

    chore: remove hardcoded

--- src/config/dataSources.ts
+++ src/config/dataSources.ts
@@ -12,7 +12,7 @@ export const supportsMorphoApi = (network: SupportedNetworks): boolean => {
     case SupportedNetworks.Arbitrum:
     case SupportedNetworks.HyperEVM:
     case SupportedNetworks.Monad:
-      return false;
+      return true;
 
     default:
       return false;
