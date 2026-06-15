41a19eeffb0101f4dac442d16cb760accddbacfc
Anton Cheng <antonassocareer@gmail.com>
2026-06-15 22:20:17 +0800

    Fix Ultracite config and image module types (#579)

--- custom.d.ts
+++ custom.d.ts
@@ -2,3 +2,43 @@ declare module '*.svg' {
   const src: string;
   export default src;
 }
+
+declare module '*.png' {
+  const src: import('next/dist/shared/lib/image-external').StaticImageData;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: import('next/dist/shared/lib/image-external').StaticImageData;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: import('next/dist/shared/lib/image-external').StaticImageData;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: import('next/dist/shared/lib/image-external').StaticImageData;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: import('next/dist/shared/lib/image-external').StaticImageData;
+  export default src;
+}
+
+declare module '*.avif' {
+  const src: import('next/dist/shared/lib/image-external').StaticImageData;
+  export default src;
+}
+
+declare module '*.ico' {
+  const src: import('next/dist/shared/lib/image-external').StaticImageData;
+  export default src;
+}
+
+declare module '*.bmp' {
+  const src: import('next/dist/shared/lib/image-external').StaticImageData;
+  export default src;
+}
