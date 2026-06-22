9fb986d1ff24c0f2e3875fc8cada1e7e1a16da9c
Anton Cheng <antonassocareer@gmail.com>
2026-06-22 23:39:47 +0800

    fix: support Safe wallet connections (#591)

--- next.config.js
+++ next.config.js
@@ -17,6 +17,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, content-type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
   // temp fix for reown package issue: https://github.com/MetaMask/metamask-sdk/issues/1376
   webpack: (config) => {
     config.resolve.fallback = {
